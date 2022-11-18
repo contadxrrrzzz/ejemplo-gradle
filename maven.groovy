74176bcc2a02a928aa81bd62415dde1611b252db
Brenda Contador <contadorz.brenda@gmail.com>
2022-11-18 16:51:21 -0300

    se agregan funciones mavn groovy

--- maven.groovy
+++ maven.groovy
@@ -3,3 +3,14 @@ def maven_build()
     sh 'mvn clean install -e'
 }
 return this
+
+def maven_compile()
+{
+    sh "./mvnw clean compile -e"
+}
+return this
+def maven_test()
+{
+    sh "./mvnw clean test -e"
+}
+return this
