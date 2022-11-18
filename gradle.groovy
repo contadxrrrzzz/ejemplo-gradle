1d63ab5aa4f302696b3511cd071c6a5c894b7a69
contadxrrrzzz <71387693+contadxrrrzzz@users.noreply.github.com>
2022-11-18 20:24:33 -0300

    Update gradle.groovy

--- gradle.groovy
+++ gradle.groovy
@@ -15,6 +15,13 @@ def gradle_run(){
     sleep 20
   }
   
+def gradle_sonar()
+{
+    withSonarQubeEnv('Sonar') { // Will pick the global server connection you have configured
+      sh './gradlew sonarqube'
+    }
+}
+
 
 return this
 
