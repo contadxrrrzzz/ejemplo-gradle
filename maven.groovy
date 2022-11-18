f246fe8711fb0b8dbcfbdc52861478224c14e667
contadxrrrzzz <71387693+contadxrrrzzz@users.noreply.github.com>
2022-11-18 19:15:37 -0300

    Update maven.groovy
    se agregan stages

--- maven.groovy
+++ maven.groovy
@@ -1,16 +1,26 @@
-def maven_build()
-{
-    sh 'mvn clean install -e'
-}
-return this
-
 def maven_compile()
 {
-    sh "./mvnw clean compile -e"
+    sh './mvnw clean compile -e'
 }
-return this
+
 def maven_test()
 {
-    sh "./mvnw clean test -e"
+   sh './mvnw clean test -e'
 }
+
+def maven_jar()
+{
+   sh './mvnw clean package -e'
+}
+
+def maven_run()
+{
+    sh 'nohup bash mvnw spring-boot:run &'
+}
+def maven_testApp()
+{
+    sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+}
+
+
 return this
