44d25ff5eddd8d5368f58eb7e11bbef7320320cb
Brenda Contador <contadorz.brenda@gmail.com>
2022-11-18 16:56:39 -0300

    se agregan funciones gradle en groovy

--- gradle.groovy
+++ gradle.groovy
@@ -1,1 +1,22 @@
+def gradle_build()
+{
+     sh 'gradle build'
+}
+return this
 
+def gradle_check()
+{
+   sh 'gradle check'
+}
+return this
+def gradle_clean()
+{
+    sh "gradle clean"
+}
+return this
+
+def gradle_run()
+{
+    sh "gradle run"
+}
+return this
